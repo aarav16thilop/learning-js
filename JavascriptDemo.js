83ad6f6223e57ae0da961e3b02906fa39a46dd1b
aarav16thilop <53447821+aarav16thilop@users.noreply.github.com>
2021-01-06 19:21:52 -0500

    jQuery and randomly generated hex colors appear onclick

--- JavascriptDemo.js
+++ JavascriptDemo.js
@@ -1,26 +1,29 @@
 var name = prompt("What is your name?");
-
+var ta = "Try again";
 if(isNaN(name) == false){
-   alert("Try again")
+   alert(ta)
 
-}else{alert("Hey "+ name +" Click on the empty box multiple times!")
+}else if (name == null){
+   alert(ta);
+
+}else {alert("Hey "+ name +"! Click on the empty box multiple times!")
 
 };
 
 
-var changeColor = document.getElementById("color_changer");
+
+   var changeColor = document.getElementById("color_changer");
 var colors = ["red", "blue", "green", "yellow"];
 var counter = 0;
 
 changeColor.onclick = function colorChanger(){
-     if(counter > 3){
+     if(counter > 0){
         counter = 0;
      };
 
-     changeColor.style.background = colors[counter];
-        counter++
+     changeColor.style.background = '#'+Math.floor(Math.random()*16777215).toString(16);
+        counter++;
 
-}
+};
 
-setInterval(colorChanger, 3000);
 
